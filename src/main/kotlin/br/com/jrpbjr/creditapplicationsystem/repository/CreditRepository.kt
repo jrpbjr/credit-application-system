c1e1b92aeb8a3301815afa3a96dbbf34e34290ae
jrpbjr <jrpbjr@gmail.com>
2025-05-23 16:21:36 -0300

    Implementacao da interface - CreditService (findByCreditCode)

--- src/main/kotlin/br/com/jrpbjr/creditapplicationsystem/repository/CreditRepository.kt
+++ src/main/kotlin/br/com/jrpbjr/creditapplicationsystem/repository/CreditRepository.kt
@@ -3,8 +3,11 @@ package br.com.jrpbjr.creditapplicationsystem.repository
 import br.com.jrpbjr.creditapplicationsystem.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
 interface CreditRepository: JpaRepository<Credit, Long> {
+
+    fun findByCreditCode(creditCode: UUID): Credit?
 
 }
