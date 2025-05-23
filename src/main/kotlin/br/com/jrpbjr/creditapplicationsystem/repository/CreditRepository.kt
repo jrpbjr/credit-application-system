40809efcd828b85bbf83cc2ea3565dd4854f700a
jrpbjr <jrpbjr@gmail.com>
2025-05-23 16:38:00 -0300

    Implementacao da interface - CreditService (findByCustomer)

--- src/main/kotlin/br/com/jrpbjr/creditapplicationsystem/repository/CreditRepository.kt
+++ src/main/kotlin/br/com/jrpbjr/creditapplicationsystem/repository/CreditRepository.kt
@@ -2,6 +2,7 @@ package br.com.jrpbjr.creditapplicationsystem.repository
 
 import br.com.jrpbjr.creditapplicationsystem.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
@@ -10,4 +11,6 @@ interface CreditRepository: JpaRepository<Credit, Long> {
 
     fun findByCreditCode(creditCode: UUID): Credit?
 
+    @Query("SELECT * FROM CREDIT WHERE CUSTOMER_ID = ?1", nativeQuery = true)
+    fun findAllByCustomerId(customerId: Long) : List<Credit>
 }
