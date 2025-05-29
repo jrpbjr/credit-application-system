ed001264a67c1856c6828351dcf7b1f74a9839e5
jrpbjr <jrpbjr@gmail.com>
2025-05-29 07:25:21 -0300

    Configuracao inicial para testes

---
+++ src/test/kotlin/br/com/jrpbjr/creditapplicationsystem/service/CustomerServiceTest.kt
@@ -0,0 +1,10 @@
+package br.com.jrpbjr.creditapplicationsystem.service
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension::class)
+class CustomerServiceTest {
+}
